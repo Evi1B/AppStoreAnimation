8b39080880518a7b7f6a66ce68f9440b082c54ea
evilb <evilb@syneforge.com>
2022-04-08 19:06:18 +0300

    add images and model

---
+++ AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by evilb on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreAnimation/Model/Today.swift
@@ -0,0 +1,25 @@
+//
+//  Today.swift
+//  AppStoreAnimation
+//
+//  Created by evilb on 08.04.2022.
+//
+
+import SwiftUI
+
+// MARK: Data Model and Sample Data
+
+struct Today: Identifiable {
+    var id = UUID().uuidString
+    var appName: String
+    var appDescriprion: String
+    var appLogo: String
+    var bannerTitle: String
+    var platformTitle: String
+    var artwork: String
+}
+
+var todayItems: [Today] = [
+    Today(appName: "Forza Horizon 5", appDescriprion: "Racing Game", appLogo: "Logo1", bannerTitle: "You're in charge of the Horizon Festival", platformTitle: "Apple Arcade", artwork: "Post1"),
+    Today(appName: "Horizon: Forbidden West", appDescriprion: "Action/RPG", appLogo: "Logo2", bannerTitle: "Join Aloy as she braves the Forbidden West – a majestic but dangerous frontier that conceals mysterious new threats.", platformTitle: "Apple Arcade", artwork: "Post2")
+]
