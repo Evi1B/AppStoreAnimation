0178e44208b62b80d2e99d8ad69b71e70b8fe1da
evilb <evilb@syneforge.com>
2022-04-08 21:05:59 +0300

    setup detail view and closing animation

--- AppStoreAnimation/View/Home.swift
+++ AppStoreAnimation/View/Home.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct Home: View {
+    // MARK: Animation Properties
+    @State var currentItem: Today?
+    @State var showDetailPage: Bool = false
+    
+    // Matched Geometry Effect
+    @Namespace var animation
+    
+    // MARK: Detail Animation Properties
+    @State var animateView: Bool = false
+    @State var animateContent: Bool = false
+    @State var scrollOffset: CGFloat = 0
+    @State var offset: CGFloat = 0
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators:  false) {
-            VStack(spacing: 30) {
+            VStack(spacing: 0) {
                 HStack(alignment: .bottom) {
                     VStack(alignment: .leading, spacing: 8) {
                         Text("MONDAY 4 APRIL")
@@ -31,17 +44,41 @@ struct Home: View {
                     
                 }
                 .padding(.horizontal)
+                .padding(.bottom)
+                .opacity(showDetailPage ? 0 : 1)
                 
                 ForEach(todayItems) { item in
                     Button {
-                        
+                        withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                            currentItem = item
+                            showDetailPage = true
+                        }
                     } label: {
                         CardView(item: item)
+                        // For Matched Geometry Effect We Didnt applied Padding
+                        // Instead Applying Scaling
+                        // Approx Scaling Value to match padding
+                            .scaleEffect(currentItem?.id == item.id && showDetailPage ? 1 : 0.93)
                     }
                     .buttonStyle(ScaledButtonStyle())
+                    .opacity(showDetailPage ? (currentItem?.id == item.id ? 1 : 0) : 1)
                 }
             }
             .padding(.vertical)
+        }
+        .overlay {
+            if let currentItem = currentItem, showDetailPage {
+                DetailView(item: currentItem)
+                    .ignoresSafeArea(.container, edges: .top)
+            }
+        }
+        .background(alignment: .top) {
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color("BG"))
+                .frame(height: animateView ? nil : 350, alignment: .top)
+                .scaleEffect(animateView ? 1 : 0.93)
+                .opacity(animateView ? 1 : 0)
+                .ignoresSafeArea()
         }
     }
     
@@ -81,6 +118,7 @@ struct Home: View {
                 }
                 .foregroundColor(.primary)
                 .padding()
+                .offset(y: currentItem?.id == item.id && animateView ? safeArea().top : 0)
             }
             
             HStack(spacing: 12) {
@@ -126,6 +164,103 @@ struct Home: View {
             RoundedRectangle(cornerRadius: 15, style: .continuous)
                 .fill(Color("BG"))
         }
+        .matchedGeometryEffect(id: item.id, in: animation)
+    }
+    
+    // MARK: Detail View
+    func DetailView(item: Today) -> some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                CardView(item: item)
+                    .scaleEffect(animateView ? 1 : 0.93)
+                
+                VStack(spacing: 15) {
+                    Text(item.appText)
+                        .multilineTextAlignment(.leading)
+                        .lineSpacing(10)
+                        .padding(.bottom, 20)
+                    
+                    Divider()
+                    
+                    Button {
+                        
+                    } label: {
+                        Label {
+                            Text("Share Story")
+                        } icon: {
+                            Image(systemName: "square.and.arrow.up.fill")
+                        }
+                        .foregroundColor(.primary)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 25)
+                        .background {
+                            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                .fill(.ultraThinMaterial)
+                        }
+                    }
+
+                }
+                .padding()
+//                .offset(y: scrollOffset > 0 ? scrollOffset : 0)
+                .opacity(animateContent ? 1 : 0)
+                .scaleEffect(animateView ? 1 : 0, anchor: .top)
+                .onChange(of: offset) { newValue in
+                    // YOUR OWN CUSTOM THERSOLD
+                    if newValue > 120 {
+                        withAnimation(.easeInOut) {
+//                            currentItem = nil
+//                            showDetailPage = false
+                            animateView = false
+                            animateContent = false
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                            withAnimation(.easeInOut) {
+//                                currentItem = nil
+//                                showDetailPage = false
+                                currentItem = nil
+                                showDetailPage = false
+                            }
+                        }
+                    }
+                }
+            }
+//            .offset(y: scrollOffset > 0 ? -scrollOffset : 0)
+            .modifier(OffsetModifier(offset: $offset))
+            .offset(offset: $scrollOffset)
+        }
+        .coordinateSpace(name: "SCROLL")
+        .overlay(alignment: .topTrailing, content: {
+            Button {
+                // Closing View
+                withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                    animateView = false
+                    animateContent = false
+                }
+                
+                withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7).delay(0.05)) {
+                    currentItem = nil
+                    showDetailPage = false
+                }
+            } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .padding(.top, safeArea().top)
+            .offset(y: -10)
+            .opacity(animateView ? 1 : 0)
+        })
+        .onAppear {
+            withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
+                animateView = true
+            }
+            withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7).delay(0.1)) {
+                animateContent = true
+            }
+        }
+        .transition(.identity)
     }
 }
 
@@ -142,5 +277,41 @@ struct ScaledButtonStyle: ButtonStyle {
         configuration.label
             .scaleEffect(configuration.isPressed ? 0.94 : 1)
             .animation(.easeInOut, value: configuration.isPressed)
+    }
+}
+
+// Safe Area Value
+extension View {
+    func safeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return . init() }
+        
+        guard let safeArea = screen.windows.first?.safeAreaInsets else { return .zero}
+        
+        return safeArea
+    }
+    
+    // MARK: ScrollView Offset
+    func offset(offset: Binding<CGFloat>) -> some View {
+        return self
+            .overlay {
+                GeometryReader { proxy in
+                    let minY = proxy.frame(in: .named("SCROLL")).minY
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: minY)
+                }
+                .onPreferenceChange(OffsetKey.self) { value in
+                    offset.wrappedValue = value
+                }
+            }
+    }
+}
+
+// MARK: Offset Key
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
---
+++ AppStoreAnimation/View/OffsetModifier.swift
@@ -0,0 +1,38 @@
+//
+//  OffsetModifier.swift
+//  AppStoreAnimation
+//
+//  Created by evilb on 08.04.2022.
+//
+
+import SwiftUI
+
+// MARK: ScrollView Offset Reader
+struct OffsetModifier: ViewModifier {
+    @Binding var offset: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                GeometryReader { proxy in
+                    let minY = proxy.frame(in: .named("SCROLL")).minY
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: minY)
+                }
+                .onPreferenceChange(OffsetKey.self) { minY in
+                    self.offset = minY
+                }
+            }
+    }
+}
+
+//struct OffsetKey: PreferenceKey {
+//    static var defaultValue: CGFloat = 0
+//
+//    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+//        value = nextValue()
+//    }
+//
+//}
+
